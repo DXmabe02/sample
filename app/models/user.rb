59703b2a1756050f06d3f3ad1c33e373d65167b6
mabe02 <watanabe02masahide02@gmail.com>
2019-02-01 21:25:24 +0000

    add a validation(format)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length:{maximum: 50}
-  validates :email, presence: true, length:{maximum: 252}
+  VALD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length:{maximum: 252}, format:{with: VALD_EMAIL_REGEX}
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,12 +29,22 @@ RSpec.describe User, type: :model do
    expect(@user.valid?).to be_falsey
  end
 
-specify 'email validation should accept valid addresses' do
-  valid_addresses = %w[example@user,com user_at_foo.org]
-  valid_addresses.each do |invalid_address|
-    @user.email = invalid_address
-    expect(@user.valid?).to be_truthy, "#{invalid_address.inspect} should be valid"
+  specify 'email validation should accept valid addresses' do
+    valid_addresses = %w[example@user.com user@foo.com]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      expect(@user.valid?).to be_truthy, "#{valid_address.inspect} should be valid"
+      #inspect配列のリテラルを出力
+    end
   end
-end
+
+  specify 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[example@user,com user_at_foo.org]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      expect(@user.valid?).to be_falsey, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
 
 end
