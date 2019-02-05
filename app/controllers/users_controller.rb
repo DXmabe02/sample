677a08ca376c4bff5b8acb6b13e5f285aace9127
mabe02 <watanabe02masahide02@gmail.com>
2019-02-05 21:28:05 +0000

    add strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    #@user = User.new(params[:user])マスアサインメントの脆弱性より、←はダメ
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+private
+ sef user_params
+   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+ end
+
+
 end
